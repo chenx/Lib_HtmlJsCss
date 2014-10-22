77e2ed524b5a6d88e9d734fc0dcfe58f410a54e9
Thomas <txchen@gmail.com>
2014-10-22 13:30:24 -1000

    more optimization on slider.js

--- slider1/js/slider/slider.js
+++ slider1/js/slider/slider.js
@@ -14,41 +14,37 @@ $(document).ready(function() {
     var slider_ct = 2;    // Number of images.
     var slider_w = 1000;  // Image width. unit: px.
     var slider_auto_interval = 10000; // Automatic slide interval (in ms). If <= 0, No automatic slide.
-    var show_bullets = 1; // 1 - show, 0 - hide.
-
-    // Calculation constants and parameters.
-    var left_limit = 0;   // Left border.
-    var right_limit = - slider_w * (slider_ct-1);  // Right border.
-    var index0 = 0;       // Current position (index of current image).
+    var slider_show_bullets = 1; // 1 - show, 0 - hide.
+    var slider_p0 = 0;    // Current position (index of current image).
 
     // Initialize controller bar, which consists of bullets, each representing an image.
-    if (show_bullets && slider_ct > 1) {
+    if (slider_show_bullets && slider_ct > 1) {
         init_slide1_controller(slider_ct);
     }
 
     // When left arrow is clicked, slide left. If already at left end, rotate to right end.
     $('.go_left').click(function(){
-      var p = (index0 == 0) ? slider_ct - 1 : index0 - 1; // new position of index.
-      arrow_onclick(index0, p);
-      index0 = p;
+      var p = (slider_p0 == 0) ? slider_ct - 1 : slider_p0 - 1; // new position of index.
+      arrow_onclick(slider_p0, p);
+      slider_p0 = p;
     });
 
     // When right arrow is clicked, slide right. If already at right end, rotate to left end.
     $('.go_right').click(function(){
-      var p = (index0 == slider_ct - 1) ? 0 : index0 + 1;
-      arrow_onclick(index0, p);
-      index0 = p;
+      var p = (slider_p0 == slider_ct - 1) ? 0 : slider_p0 + 1;
+      arrow_onclick(slider_p0, p);
+      slider_p0 = p;
     });
 
     // If a bullet is clicked, slide to corresponding image.
     $('.bullet').click(function(){
-        //alert(index0 + ' -> ' + $(this).index());
-        index0 = $(this).index();
-        bullet_onclick(index0);
+        //alert(slider_p0 + ' -> ' + $(this).index());
+        slider_p0 = $(this).index();
+        bullet_onclick(slider_p0);
     });
     $('.select').click(function(){
-        var index0 = $(this).index();
-        bullet_onclick(index0);
+        var slider_p0 = $(this).index();
+        bullet_onclick(slider_p0);
     });
 
     // Automatic slide, starting from beginning. Interval is slider_auto_interval/1000 seconds.
