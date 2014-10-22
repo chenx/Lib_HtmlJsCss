002c2d1a79ed277c362218587a0ebd1a711aac15
Thomas <txchen@gmail.com>
2014-10-22 13:23:03 -1000

    optimized silder.js

--- slider1/js/slider/slider.js
+++ slider1/js/slider/slider.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     // The top 4 parameters are for configuration.
     var slider_ct = 2;    // Number of images.
     var slider_w = 1000;  // Image width. unit: px.
-    var slider_auto_interval = 10000; // Automatic slide interval. If <= 0, don't slide automatically. Unit: ms.
+    var slider_auto_interval = 10000; // Automatic slide interval (in ms). If <= 0, No automatic slide.
     var show_bullets = 1; // 1 - show, 0 - hide.
 
     // Calculation constants and parameters.
@@ -28,46 +28,27 @@ $(document).ready(function() {
 
     // When left arrow is clicked, slide left. If already at left end, rotate to right end.
     $('.go_left').click(function(){
-      var p = $("#slide1_images").position().left;
-      p = parseInt(p / slider_w) * slider_w; // avoid partial p if automatic slided.
-      if (p >= left_limit) { p = right_limit; select_bullet( slider_ct - 1 ); }
-      else { p += slider_w; select_bullet( - p/slider_w ); }
-      $("#slide1_images").css("transform","translateX(" + p + "px)");
+      var p = (index0 == 0) ? slider_ct - 1 : index0 - 1; // new position of index.
+      arrow_onclick(index0, p);
+      index0 = p;
     });
 
     // When right arrow is clicked, slide right. If already at right end, rotate to left end.
     $('.go_right').click(function(){
-      var p = $("#slide1_images").position().left;
-      p = parseInt(p / slider_w) * slider_w; // avoid partial p if automatic slided.
-      if (p <= right_limit) { p = left_limit; select_bullet(0); }
-      else { p -= slider_w; select_bullet( - p/slider_w ); }
-      $("#slide1_images").css("transform","translateX(" + p + "px)");
+      var p = (index0 == slider_ct - 1) ? 0 : index0 + 1;
+      arrow_onclick(index0, p);
+      index0 = p;
     });
 
     // If a bullet is clicked, slide to corresponding image.
     $('.bullet').click(function(){
-        var index = $(this).index();
-        //alert(index0 + ' -> ' + index);
-
-        var p = - (index) * slider_w;
-        $("#slide1_images").css("transform","translateX(" + p + "px)");
-        index0 = index;
-
-        clear_select();
-        $(this).removeClass("bullet");
-        $(this).addClass("select");
+        //alert(index0 + ' -> ' + $(this).index());
+        index0 = $(this).index();
+        bullet_onclick(index0);
     });
     $('.select').click(function(){
-        var index = $(this).index();
-        //alert(index);
-
-        var p = - (index) * slider_w;
-        $("#slide1_images").css("transform","translateX(" + p + "px)");
-        index0 = index;
-
-        clear_select();
-        $(this).removeClass("bullet");
-        $(this).addClass("select");
+        var index0 = $(this).index();
+        bullet_onclick(index0);
     });
 
     // Automatic slide, starting from beginning. Interval is slider_auto_interval/1000 seconds.
@@ -75,36 +56,39 @@ $(document).ready(function() {
         setInterval("$('.go_right').click();", slider_auto_interval); 
         //(function() { setInterval("$('.go_right').click();", 5000);  }); // this doesn't work.
     }
+
+
+    function arrow_onclick(p0, p) {
+        $('#slide1_controller').children('span').eq(p0).removeClass("select");
+        $('#slide1_controller').children('span').eq(p).addClass("select");
+
+        p *= - slider_w;
+        $("#slide1_images").css("transform","translateX(" + p + "px)");
+    }
+
+    function bullet_onclick(p) {
+        // Clear all bullets to non-selected mode.
+        $('#slide1_controller').children('span').removeClass("select");
+
+        // set current bullet class.
+        $('#slide1_controller').children('span').eq(p).removeClass("bullet");
+        $('#slide1_controller').children('span').eq(p).addClass("select");
+
+        p *= - slider_w;
+        $("#slide1_images").css("transform","translateX(" + p + "px)");
+    }
+
+    // Initialize the controller bar.
+    function init_slide1_controller(slider_ct) {
+        $("#slide1_controller").append(
+            '<span style="right:' + (slider_ct * 30) + 'px;" class="select"></span>');
+        for (var i = 1; i < slider_ct; ++ i) {
+            $("#slide1_controller").append(
+                '<span style="right:' + ((slider_ct - i) * 30) + 'px;" class="bullet"></span>');
+        }
+    }
 });
 
 
-// Initialize the controller bar.
-function init_slide1_controller(slider_ct) {
-    $("#slide1_controller").append('<span style="right:' + (slider_ct * 30) + 'px;" class="select"></span>');
-    for (var i = 1; i < slider_ct; ++ i) {
-        $("#slide1_controller").append('<span style="right:' + ((slider_ct - i) * 30) + 'px;" class="bullet"></span>');
-    }
-}
 
-// Clear all bullets to non-selected mode.
-function clear_select() {
-    $('#slide1_controller').find('span').each(function() { 
-        $(this).removeClass("select");
-    });
-}
-
-// Select given bullet.
-function select_bullet(index) {
-    var idx = parseInt(index);
-    index0 = idx; // update current index.
-    //alert(idx);
-    clear_select();
-
-    $('#slide1_controller').find('span').each(function() {
-        if (idx == 0) {
-            $(this).addClass("select");
-        }
-        idx --;
-    });
-}
 
